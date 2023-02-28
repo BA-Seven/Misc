4e4f9d10280bb541b508d3ab98d3b57d45b2eaed
Seven <84856061+BA-Seven@users.noreply.github.com>
2023-02-28 17:10:20 +0100

    Update un_char_sur_deux.py

--- un_char_sur_deux.py
+++ un_char_sur_deux.py
@@ -1,7 +1,5 @@
 """Besoin de faire du phishing avec une xss ? URLencode c'est bien mais le lien deviens vite long. URLencode un char sur 2 et ton url sera leggit pour l'average user."""
 
-
-
 string = "Ceci est une chaîne à encoder"
 
 encoded = ""
